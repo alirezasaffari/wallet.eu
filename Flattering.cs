ac4ddeb4e44472708e27410341774f3c9ac38cb4
Alireza Saffari <greenapple.1910@live.com>
2022-09-14 22:16:36 +0430

    Solution was added

--- Flattering.cs
+++ Flattering.cs
@@ -1,8 +1,17 @@
 ﻿namespace Flattering;
+
 public static class Flatter
 {
+    public static List<object> results = new();
     public static List<object> MakeFlat(List<object> input)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < input.Count; i++)
+			{
+            if(input[i] is int)
+                results.Add(input[i]);
+                    else
+                MakeFlat((List<object>)input[i]);
+			}
+        return results;
     }
 }
